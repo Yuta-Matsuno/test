8d4844ec9af3435352e46db8f8e5aa8f4711d598
Yuta-Matsuno <41985391+Yuta-Matsuno@users.noreply.github.com>
2018-09-11 13:04:31 +0900

    Update execshell.py

--- simple-python-app/execshell.py
+++ simple-python-app/execshell.py
@@ -1,12 +1,13 @@
 import os
 
-#while True:
-#    counter += 1
-answer = os.system('./shell.sh')
+while True:
+  counter += 1
+  answer = os.system('./shell.sh')
 
-if　answer == 0:
-  print('Success')
+  if answer == 0:
+    print('Success')
+    os.rename('./show-run.log', './show-run_' + '{0:02d}'.format(counter) + '.log')
   else:
-  print('Fail')
-#if counter == 3:
-#   break
+    print('Fail')
+  if counter == 5:
+    break
